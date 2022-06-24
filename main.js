858478b16cb9711a085a9b481f07fe20b776c1d2
Moss <danielsmoss96@gmail.com>
2022-06-24 14:51:05 +0100

    Added trajectory and initial angle being drawn, setup for printing max height coordinates and angles in place

--- main.js
+++ main.js
@@ -12,16 +12,23 @@ const speedZ = document.getElementById("inputZ")
 const rotationX =  document.getElementById("inputXR")
 const rotationY =  document.getElementById("inputYR")
 const inputters = document.getElementsByClassName("inputter")
+const calculate = document.getElementsByClassName("calculate")
 
+//CONSTANTS
+const PI = Math.PI;
+const radToDeg = 180/PI;
 //Variables
 const tickRate = 1000 //ticks per second
 
 //Add event listeners
-//for (let i = 0; i < inputters.length; i++) 
-inputters[4].addEventListener('click', () => {
-  calculateTrajectory()
-}) //Can add something here to invoke on update to variables
+for (let i = 0; i < inputters.length; i++) {
+  inputters[i].addEventListener('input', () => {drawArrow()})
+}
 
+calculate[0].addEventListener('click', () => {
+  if (particle.position.y !== 0) return;
+  calculateTrajectory()
+})
 
 
 
@@ -65,34 +72,80 @@ function animate() {
   renderer.render(scene, camera);
 }
 
+const arrowMat = new THREE.LineBasicMaterial({ color: 0x00ffff })
+const arrowPoints = [];
+arrowPoints.push( new THREE.Vector3(0, 0, 0))
+let currentUUID = ''
+function drawArrow() {
+  if (currentUUID) {
+    const oldArrow = scene.getObjectByProperty('uuid', currentUUID);
+    oldArrow.geometry.dispose();
+    oldArrow.material.dispose();
+    scene.remove(oldArrow);
+  }
+  const xSpeed = speedX.value ? parseInt(speedX.value): 0;
+  const ySpeed = speedY.value ? parseInt(speedY.value) : 0;
+  const zSpeed = speedZ.value ? parseInt(speedZ.value): 0;
+
+  const initialVelocityXY = new THREE.Vector2(xSpeed, ySpeed);
+  const initialAngleXY = Math.atan(ySpeed/xSpeed) * radToDeg;
+  const initialVelocityXZ = new THREE.Vector2(xSpeed, zSpeed);
+  const initialAngleXZ = Math.atan(zSpeed/xSpeed) * radToDeg;
+
+  console.log('Y: ' + initialAngleXY)
+  console.log('Z: ' + initialAngleXZ)
+
+  arrowPoints[1] = new THREE.Vector3(xSpeed, ySpeed, zSpeed);
+  let geometry = new THREE.BufferGeometry().setFromPoints( arrowPoints );
+  const arrow = new THREE.Line( geometry, arrowMat );
+
+
+
+  scene.add(arrow);
+  currentUUID = arrow.uuid;
+  console.log(arrow)
+}
+
+drawArrow()
+
 function calculateTrajectory() {
   let objectMass = mass.value ? parseInt(mass.value) : 1; 
   let xSpeed = speedX.value ? parseInt(speedX.value): 0;
-  let ySpeed = speedY.value ? parseInt(speedY.value) * 10 : 0;
+  let ySpeed = speedY.value ? parseInt(speedY.value) : 0;
   let zSpeed = speedZ.value ? parseInt(speedZ.value): 0;
-  let maxHeight = 0;
+  let maxHeight = [0, 0, 0];
 
-  const initialVelocity = new THREE.Vector2(xSpeed, ySpeed)
-  const initialAngle = Math.atan(ySpeed/xSpeed)
-
-  console.log(initialVelocity, THREE.MathUtils.radToDeg(initialAngle))
+  const lineMat = new THREE.LineBasicMaterial({ color: 0x0000ff })
+  const points = [];
+  points.push( new THREE.Vector3(0, 0, 0))
+  let geometry = new THREE.BufferGeometry().setFromPoints( points );
 
   function oneTick() {
-    particle.position.y += ySpeed / (tickRate);
+    particle.position.y += ySpeed / (tickRate / 10);
     particle.position.x += xSpeed / (tickRate / 10);
     particle.position.z += zSpeed / (tickRate / 10);
+    //Draw trajectory line
+    points.push( new THREE.Vector3( particle.position.x, particle.position.y, particle.position.z) );
+    geometry = new THREE.BufferGeometry().setFromPoints( points );
+    const line = new THREE.Line( geometry, lineMat );
+    scene.add(line);
 
-    ySpeed -= 9.81 * objectMass / (tickRate / 10 );
+    ySpeed -= 9.81 * objectMass / (tickRate);
 
-    if (particle.position.y > maxHeight) maxHeight = particle.position.y
+    if (particle.position.y > maxHeight[1]) {
+      maxHeight[0] = particle.position.x;
+      maxHeight[1] = particle.position.y;
+      maxHeight[2] = particle.position.z;
+    }
 
-    console.log(maxHeight + ' y')
-    console.log(particle.position.x + ' x')
     if (particle.position.y > 0) {
       setTimeout(() => {
-      oneTick()
-    }, 100 / tickRate)}
-    else (particle.position.y = 0)
+      oneTick();
+    }, 1000 / tickRate)}
+    else {
+      particle.position.y = 0;
+      returnStats();
+    }
   }
   oneTick();
 
